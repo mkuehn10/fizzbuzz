5379c9e49c0480aaf2304f3996498d04ef7289f8
Michael Kuehn <mkuehn10@gmail.com>
2016-07-13 16:28:07 -0400

    complete fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,3 +1,24 @@
-$(function () {
+$(function() {
 
+    var divByThree;
+    var divByFive;
+    var appendHTML;
+
+    for (var i = 1; i <= 100; i++) {
+
+        divByThree = (i % 3 === 0);
+        divByFive = (i % 5 === 0);
+
+        if (divByThree && divByFive) {
+            appendHTML = 'fizzbuzz';
+        } else if (divByThree) {
+            appendHTML = 'fizz';
+        } else if (divByFive) {
+            appendHTML = 'buzz';
+        } else {
+            appendHTML = i;
+        }
+
+        $('.container').append('<p class="num">' + appendHTML + '</p>');
+    }
 });
