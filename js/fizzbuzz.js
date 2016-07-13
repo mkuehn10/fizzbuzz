9b99c623b2c0967d3c934fdbb283fb34f454cebe
Michael Kuehn <mkuehn10@gmail.com>
2016-07-13 18:56:26 -0400

    finish refactor

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,24 +1,58 @@
 $(function() {
 
-    var divByThree;
-    var divByFive;
-    var appendHTML;
+    'use strict';
 
-    for (var i = 1; i <= 100; i++) {
+    function fizzBuzzify(num) {
+        var divByThree;
+        var divByFive;
+        var appendHTML;
 
-        divByThree = (i % 3 === 0);
-        divByFive = (i % 5 === 0);
+        for (var i = 1; i <= num; i++) {
+            console.log(i);
+            divByThree = (i % 3 === 0);
+            divByFive = (i % 5 === 0);
 
-        if (divByThree && divByFive) {
-            appendHTML = 'fizzbuzz';
-        } else if (divByThree) {
-            appendHTML = 'fizz';
-        } else if (divByFive) {
-            appendHTML = 'buzz';
+            if (divByThree && divByFive) {
+                appendHTML = 'fizzbuzz';
+            } else if (divByThree) {
+                appendHTML = 'fizz';
+            } else if (divByFive) {
+                appendHTML = 'buzz';
+            } else {
+                appendHTML = i;
+            }
+
+            $('.container').append('<p class="num">' + appendHTML + '</p>');
+        }
+    }
+
+
+    $('#fizzbuzz-form').submit(function(event) {
+        event.preventDefault();
+        $('.container').html('');
+        var numToFizzBuzz = $('#fizzbuzz-number').val();
+
+        console.log(parseInt(numToFizzBuzz) == numToFizzBuzz);
+        if (parseInt(numToFizzBuzz) == numToFizzBuzz) {
+            $('#fizzbuzz-number').val(parseInt(numToFizzBuzz));
+            $('.error-message').hide();
+            $('#fizzbuzz-number').removeClass('red-border');
+            fizzBuzzify(numToFizzBuzz);
         } else {
-            appendHTML = i;
+            $('.error-message').show();
+            $('#fizzbuzz-number').addClass('red-border');
         }
 
-        $('.container').append('<p class="num">' + appendHTML + '</p>');
-    }
+    });
+
+    $('#fizzbuzz-number').keypress(function(event) {
+        if (event.keyCode === 13) {
+            $('#fizzbuzz-form').submit();
+            return false;
+        }
+    });
+
+    $('.close').click(function() {
+        $('.alert').hide();
+    });
 });
